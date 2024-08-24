468bab7f1ab6862a49d31d061d65364501db1f87
Ayush <143862828+Ayushsingla1@users.noreply.github.com>
2024-08-25 01:56:22 +0530

    config added

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require('express')
 const app = express();
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const db = require('./config/db')
 require('dotenv').config();
 
-app.listen(process.env.PORT || 3000, )
+db();
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser())
+app.listen(process.env.PORT || 3000, 
+    console.log("hello world")
+)
